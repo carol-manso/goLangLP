7e4d1bdd6bdaff445db80d69c70a8182cb050c9b
Joao Victor Amorim Vieira <amorimvictorjoao3@gmail.com>
2021-04-17 12:12:05 -0300

    adicao leituras

--- ExemploProgramas/fmt/fmt.go
+++ ExemploProgramas/fmt/fmt.go
@@ -1,11 +1,25 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
 )
 
+
+
+
 func main() {
-	x := "oi, bom dia\n como vai? \n \" que \" " ///string interpretada-> \n, \" São as interpret strings literalsão interpretados.
+	
+	//fmtEscrita()
+	fmtLeitura()
+	
+	
+}
+
+func fmtEscrita() {
+
+	x := "oi, bom dia\n como vai? \n \" que \" " ///string interpretada-> \n, \" São as interpret strings literal são interpretados.
 	y := `oiii              \n      \" blz?`     // raw = cru. Não interpreta nada do que está lá dentro.
 
 	fmt.Println(x)
@@ -13,6 +27,37 @@ func main() {
 	x = "oi"
 	y = "bom dia"
 	fmt.Print(x)               //não tem uma linha nova
-	z := fmt.Sprint(x, " ", y) //junto x e y em uma string
+	z := fmt.Sprint(x, " ", y) //junto x e y em uma string, formata uma string
+	
 	fmt.Println(z)
+
+
+}
+
+
+func fmtLeitura(){
+
+	reader := bufio.NewReader(os.Stdin)
+
+	
+	fmt.Print("Digite um caractere: ") 
+	entrada, _, _ := reader.ReadRune()	//le o caractere
+	reader.Discard(1)					//descarta o '\n' ao fim da linha
+	
+	//A leitura pode ocorrer de dois metodos diferentes
+	
+	fmt.Print("Digite um numero: ")
+	//input,_ := reader.ReadString('\n')	//Le a string incluindo o '\n'
+	//input = strings.TrimSpace(input)		//remove espaços e quebras de linhas
+	//inteiro,_ := strconv.Atoi(input)		//converte a string para inteiro
+
+	var inteiro int
+	fmt.Scanf("%d\n",&inteiro)
+
+	fmt.Printf("entrada = %c inteiro = %d\n",entrada,inteiro)
+
+
+
+
+
 }
